f4f9b8cc93aacfd526d77f011cf598a673f51f50
Gaurav Suthar <gaurav.suthar161@gmail.com>
2023-06-10 17:38:22 +0530

    add dark mode support

--- src/components/portfolio/index.tsx
+++ src/components/portfolio/index.tsx
@@ -1,21 +1,31 @@
-import { styled } from "../../../stitches.config";
+import { createContext, useState } from "react";
+import { darkTheme, styled } from "../../../stitches.config";
 import About from "./About";
 import Footer from "./Footer";
 import Header from "./Header";
 import Hero from "./Hero";
 import Work from "./Work";
 
-const Main = styled("main", {});
+const Main = styled("main", {
+  [`.${darkTheme} &`]: {
+    backgroundColor: "#000",
+    color: "#fff",
+  },
+});
 
 const PortFolio = () => {
+  const ThemeContext = createContext("light");
+  const [theme, setTheme] = useState("light");
   return (
-    <Main>
-      <Header />
-      <Hero />
-      <About />
-      <Work />
-      <Footer />
-    </Main>
+    <ThemeContext.Provider value={theme}>
+      <Main>
+        <Header />
+        <Hero />
+        <About />
+        <Work />
+        <Footer />
+      </Main>
+    </ThemeContext.Provider>
   );
 };
 
